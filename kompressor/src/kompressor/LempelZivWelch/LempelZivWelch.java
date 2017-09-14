c8a0fc46c7fe800c521f7eb88b6ee186ba8bc27c
otsepp <o.tuomas.seppanen@gmail.com>
2017-09-14 11:50:07 +0300

    testing

---
+++ kompressor/src/kompressor/LempelZivWelch/LempelZivWelch.java
@@ -0,0 +1,18 @@
+
+package kompressor.LempelZivWelch;
+
+import java.util.Map;
+
+public class LempelZivWelch {
+    //string - code initial dictionary  FINAL!
+   
+    
+    //code - string initial dictionary  FINAL!
+    
+    public LempelZivWelch() {
+        //initialise dictionaries
+    }
+    
+    
+    
+}
--- kompressor/src/kompressor/Main.java
+++ kompressor/src/kompressor/Main.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kompressor;
 
-/**
- *
- * @author Glarthir
- */
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Integer i = new Integer(112);
+//        String input = readInputTEST();
+    }
+    
+    public static String readInputTEST() {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Syötä merkkijono joka koostuu kirjaimista a - c :");
+        System.out.print("\t>");
+        
+        String input = s.nextLine();
+        
+        if (input.matches("[a-c]+")) {
+            try {
+                System.out.println("Merkkijonon \"" + input + "\" koko on " + input.getBytes("UTF-8").length * 8 +" bittiä.");
+            } catch (UnsupportedEncodingException e) {
+                System.out.println(e);
+            }
+        } else {
+            System.out.println("Virheellinen syöte!");
+        }
+        return input;
     }
     
 }
