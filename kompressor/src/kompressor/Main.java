8f3741acc6df431decd9a80601c31059bebe78f9
otsepp <o.tuomas.seppanen@gmail.com>
2017-09-16 11:00:40 +0300

    compress all

--- kompressor/src/kompressor/Main.java
+++ kompressor/src/kompressor/Main.java
@@ -8,16 +8,21 @@ import java.util.Map;
 
 public class Main {
 
+    static final int initalDictionarySize = 256;
+    
     public static void main(String[] args) {
-        String s = "bananabandana";
-        System.out.print(s + " -> ");
+        String s = "zululzu";
+        System.out.println(s + "(length: " + s.length() + ")");
         List<Integer> encoded = encode(s);
-        System.out.println(encoded + " -> " + (decode(encoded)));
+        String decoded = decode(encoded);
+        
+        System.out.println("-> " + encoded + " (length: " + encoded.size() + ")");
+        System.out.println("-> " + decoded + ", " + s.equals(decoded));
     }
     
     public static List<Integer> encode(String s) {
         Map<String, Integer> dictionary = initialiseEncodingDictionary();
-        int code = 25; 
+        int nextCode = initalDictionarySize + 1; 
         
         List<Integer> encoded = new ArrayList();
         
@@ -30,7 +35,7 @@ public class Main {
                 a = a + b;
             } else {
                 encoded.add(dictionary.get(a));
-                dictionary.put(a + b, ++code);
+                dictionary.put(a + b, nextCode++);
                 a = b;
             }
         }
@@ -41,7 +46,7 @@ public class Main {
     public static String decode(List<Integer> encoded) {
         Map<Integer, String> dictionary = initialiseDecodingDictionary();
         
-        int maxCode = 25;
+        int nextCode = initalDictionarySize + 1;
         
         String decoded = "";
         String prev = "";
@@ -52,29 +57,35 @@ public class Main {
             String concat = prev + current.charAt(0);
             
             if (!dictionary.containsValue(concat)) {
-                 dictionary.put(++maxCode, concat);
+                 dictionary.put(nextCode++, concat);
              }
              prev = current;
         }
         return decoded;
     }
 
-     //a-z
     public static Map<String, Integer> initialiseEncodingDictionary() {
          Map<String, Integer> dictionary = new HashMap();
-        for (int i = 0, j = 97; i <= 25; i++, j++) {
-//            System.out.println((char) j + " = " + j + " -> " + i);
-            dictionary.put((char) j + "", i);
-        }
+         for (int i = 0; i < initalDictionarySize; i++) {
+             dictionary.put((char) i + "", i);
+         }
+         //a - z
+//        for (int i = 0, j = 97; i <= 25; i++, j++) {
+////            System.out.println((char) j + " = " + j + " -> " + i);
+//            dictionary.put((char) j + "", i);
+//        }
         return dictionary;
     }
     
-    //a-z
     public static Map<Integer, String> initialiseDecodingDictionary() {
         Map<Integer, String> dictionary = new HashMap();
-        for (int i = 0; i <= 25; i++) {
-            dictionary.put(i, (char)(i+97) + "");
+        for (int i = 0; i < initalDictionarySize; i++) {
+            dictionary.put(i, (char) i + "");
         }
+        //a - z
+//        for (int i = 0; i <= 25; i++) {
+//            dictionary.put(i, (char)(i+97) + "");
+//        }
         return dictionary;
     }
    
